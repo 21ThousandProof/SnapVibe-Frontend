025513e20355e78a24c0dc5c1ba3a8eb4a9cbbec
21Cash <21ThousandProof@gmail.com>
2023-11-30 18:25:50 +0530

    Added Dark Mode

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,16 +1,58 @@
-import React from "react";
-import CreateRoom from "../CreateRoom/CreateRoom";
-import NameField from "../NameField/NameField";
+// Homepage.jsx
+
+import React, { useState } from "react";
 import InfoBox from "../InfoBox/InfoBox";
 
 const Homepage = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <>
-      {/* <NameField onNameChanged={setUsername} /> */}
-      {/* <CreateRoom /> */}
-      <InfoBox />
+      <div
+        style={{
+          backgroundColor: "#1e1e1e",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "#e6e6e6",
+        }}
+      >
+        <h1
+          style={{
+            ...styles.title,
+            fontSize: isHovered ? "100px" : "100px",
+            transform: isHovered ? "scale(1.2)" : "scale(1)",
+          }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          Snap Vibe
+        </h1>
+        <InfoBox />
+      </div>
     </>
   );
+};
+
+const styles = {
+  title: {
+    fontWeight: "bold",
+    color: "#4a90e2",
+    marginTop: "80px",
+    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)",
+    cursor: "pointer",
+    transition: "transform 0.3s ease, font-size 0.3s ease",
+  },
 };
 
 export default Homepage;
--- src/Components/InfoBox/InfoBox.jsx
+++ src/Components/InfoBox/InfoBox.jsx
@@ -3,20 +3,6 @@
 import React, { useState } from "react";
 import { createRoom, joinRoom } from "../../../REST";
 
-const customScrollbarStyle = `
-  ::-webkit-scrollbar {
-    width: 12px;
-  }
-  ::-webkit-scrollbar-thumb {
-    background-color: #3498db;
-    border-radius: 10px;
-  }
-  ::-webkit-scrollbar-track {
-    background-color: #ecf0f1;
-    border-radius: 10px;
-  }
-`;
-
 const InfoBox = () => {
   const [userName, setUserName] = useState("");
   const [roomName, setRoomName] = useState("");
@@ -30,94 +16,88 @@ const InfoBox = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "20px",
-        border: "1px solid #3498db",
-        borderRadius: "10px",
-        backgroundColor: "#ecf0f1",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-        width: "400px",
-        margin: "auto",
-        transition: "background-color 0.3s ease",
-        overflow: "auto", // Hide default scrollbar
-        scrollbarWidth: "thin", // Firefox support
-        scrollbarColor: "#3498db #ecf0f1", // Firefox support
-        ...{ WebkitOverflowScrolling: "touch" }, // Add for iOS support
-      }}
-    >
-      <style>{customScrollbarStyle}</style>
+    <div style={styles.container}>
       <input
         type="text"
         placeholder="Enter UserName"
         value={userName}
         onChange={(e) => setUserName(e.target.value)}
-        style={{
-          margin: "10px 0",
-          padding: "12px",
-          borderRadius: "6px",
-          border: "1px solid #bdc3c7",
-          width: "100%",
-          boxSizing: "border-box",
-          transition: "border-color 0.3s ease",
-        }}
+        style={styles.input}
       />
       <input
         type="text"
         placeholder="Enter RoomName"
         value={roomName}
         onChange={(e) => setRoomName(e.target.value)}
-        style={{
-          margin: "10px 0",
-          padding: "12px",
-          borderRadius: "6px",
-          border: "1px solid #bdc3c7",
-          width: "100%",
-          boxSizing: "border-box",
-          transition: "border-color 0.3s ease",
-        }}
+        style={styles.input}
       />
       <button
         onClick={handleCreateRoom}
-        style={{
-          background: "#2ecc71",
-          color: "#fff",
-          padding: "12px 20px",
-          border: "none",
-          borderRadius: "6px",
-          cursor: "pointer",
-          margin: "10px 0",
-          transition: "background-color 0.3s ease",
-          ...(roomName && {
-            backgroundColor: "#27ae60",
-          }),
-        }}
+        style={{ ...styles.button, ...styles.createButton }}
       >
         Create Room
       </button>
       <button
         onClick={handleJoinRoom}
-        style={{
-          background: "#2ecc71",
-          color: "#fff",
-          padding: "12px 20px",
-          border: "none",
-          borderRadius: "6px",
-          cursor: "pointer",
-          margin: "10px 0",
-          transition: "background-color 0.3s ease",
-          ...(roomName && {
-            backgroundColor: "#27ae60",
-          }),
-        }}
+        style={{ ...styles.button, ...styles.joinButton }}
       >
         Join Room
       </button>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "20px",
+    border: "1px solid #555", // Dark border color
+    borderRadius: "10px",
+    backgroundColor: "#333", // Dark background color
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+    width: "500px",
+    margin: "auto",
+    transition: "background-color 0.3s ease",
+    overflow: "auto",
+    scrollbarWidth: "thin",
+    scrollbarColor: "#4a90e2 #333", // Scrollbar color and dark background color
+    ...{ WebkitOverflowScrolling: "touch" },
+  },
+  input: {
+    margin: "10px 0",
+    padding: "12px",
+    borderRadius: "6px",
+    border: "1px solid #ddd", // Lighter border color
+    width: "100%",
+    boxSizing: "border-box",
+    transition: "border-color 0.3s ease",
+    background: "#fff", // Whitish input background color
+    color: "#333", // Dark text color
+  },
+  button: {
+    background: "#4a90e2",
+    color: "#fff",
+    padding: "12px 20px",
+    border: "none",
+    borderRadius: "6px",
+    cursor: "pointer",
+    margin: "10px 0",
+    transition: "background-color 0.3s ease",
+  },
+  createButton: {
+    width: "220px", // Adjust the width as needed
+    "&:hover": {
+      background: "#185a9d", // Darker blue color on hover
+    },
+  },
+  joinButton: {
+    width: "220px", // Adjust the width as needed
+    "&:hover": {
+      background: "#185a9d", // Darker blue color on hover
+    },
+  },
 };
 
 export default InfoBox;
