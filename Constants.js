eb5f5fe43dca33995a90ec37421abdbf6279c47c
21Cash <21ThousandProof@gmail.com>
2023-11-30 18:54:06 +0530

    Added Backend Url

--- Constants.js
+++ Constants.js
@@ -1,5 +1,5 @@
-let backendUrl = "http://localhost:3000/";
+// let backendUrl = "http://localhost:3000/";
 
-// let backendUrl = "https://snapvibe.onrender.com";
+let backendUrl = "https://snapvibe.onrender.com";
 
 export { backendUrl };
